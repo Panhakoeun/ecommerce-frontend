10ea1e1121aadbcc27c67d2b6479573b2b76e800
Panhakoeun <panha.koeun@student.passerellesnumeriques.org>
2026-06-23 19:59:57 +0700

    add git ingore and env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
     baseURL: `${BASE_URL}/api`,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
